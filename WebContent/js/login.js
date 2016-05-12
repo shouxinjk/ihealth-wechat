364bf2ff6290c4597541edc6f80130c26c26fa3f
yuanxiaodong <yuanxiaodong@shouxinjk.com>
2016-05-12 17:17:34 +0800

    tiaoshi

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -27,6 +27,7 @@ function register(){
            if (r.result == "success") {
         	  var userId = r.data.USER_ID;
         	  SetCookie("mobilep",mobilep,7);
+        	  alert(ReadCookie("userId")+"oldloginCookieUserid");
         	  SetCookie("userId",userId,7);
         	  alert(ReadCookie("userId")+"loginCookieUserid");
         	   window.location="http://www.shouxinjk.net/ihealth-wechat/subject/Message.html?userId="+r.data.USER_ID;
