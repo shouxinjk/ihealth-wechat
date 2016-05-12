13c3b45d2887ea9f9de3c14e37f360d863e2f69d
yuanxiaodong <yuanxiaodong@shouxinjk.com>
2016-05-12 12:19:17 +0800

    tiaoshi

--- src/com/shouxin/weixin/servlet/UserInfoServlet.java
+++ src/com/shouxin/weixin/servlet/UserInfoServlet.java
@@ -25,8 +25,8 @@ public class UserInfoServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		AccessToken accessToken = TokenThred.getAccessToken();
-		//String openId = (String) req.getSession().getAttribute("openId");
-		//System.out.println(openId+"====userInfoOpenId");
+		String openId = (String) req.getSession().getAttribute("openId");
+		System.out.println(openId+"====userInfoOpenId");
 		WeiXinUtil we = new WeiXinUtil();
 		WeiXinUserInfo userInfo = we.getUserInfo(accessToken.getAccess_token(),"o8NoVwgE-JJvYHeV548LsGAG0S1k");
 		resp.setCharacterEncoding("utf-8");
