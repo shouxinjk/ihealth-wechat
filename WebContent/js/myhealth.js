ef55218524827d3ee698c4927d4e709a70d62210
quwei <quwei@shouxinjk.com>
2016-05-13 17:38:35 +0800

    修改bug

--- WebContent/js/myhealth.js
+++ WebContent/js/myhealth.js
@@ -36,9 +36,7 @@ function relevance(userId){    //获取关联用户名
 				var data = eval(r.data);
 				var str = "";
 					for(var i=0;i<data.length;i++){
-						str+="<li id='li"+(i+1)+"' onclick='guanlian
-
-(\"li"+(i+1)+"\")'><input type='hidden' value='"+data[i].USER_ID+"'>"+data[i].NAME+"</li>";
+						str+="<li id='li"+(i+1)+"' onclick='guanlian(\"li"+(i+1)+"\")'><input type='hidden' value='"+data[i].USER_ID+"'>"+data[i].NAME+"</li>";
 					}
 				$('#my').attr("onclick","guanlian1('"+userId+"')");
 				 $('#my').after(str);
@@ -83,11 +81,7 @@ function intn(userId){
 	        success: function (r) {
 	        	var data = eval(r.data);
 	        	for(var j=0;j<group.length;j++){
-	        		var str ="<div class='touch item col-lg-12 col-xs-12 col-md-12 
-
-col-sm-12' id='touch"+j+"' userAddressId='1'><div class='jc_project col-lg-12 col-xs-12 col-
-
-md-12 col-sm-12'>";
+	        		var str ="<div class='touch item col-lg-12 col-xs-12 col-md-12col-sm-12' id='touch"+j+"' userAddressId='1'><div class='jc_project col-lg-12 col-xs-12 col-md-12 col-sm-12'>";
 	        		var k =0;
 	        		for(var i=0;i<data.length;i++){
 	        			if(group[j].SUBGROUP == data[i].SUBGROUP){
@@ -95,23 +89,13 @@ md-12 col-sm-12'>";
 	        				if(data[i].STATUS == "已选中"){
 	        					CHECKUPITEMid = data[i].CHECKUPITEM_ID;
 	        					name = data[i].NAME;
-	        					str +="<span class='active_ subgroup1 ' 
-
->"+data[i].NAME+"("+data[i].FREQUENCY+")<input type='hidden' value='"+data[i].CHECKUPITEM_ID
-
-+"'></span>"+"/";
+	        					str +="<span class='active_ subgroup1 '>"+data[i].NAME+"("+data[i].FREQUENCY+")<input type='hidden' value='"+data[i].CHECKUPITEM_ID+"'></span>"+"/";
 	        					k++;
 	        				}else{
 	        					if(k==0){
-	        						CHECKUPITEMid = data
-
-[i].CHECKUPITEM_ID;
+	        						CHECKUPITEMid = data[i].CHECKUPITEM_ID;
 	        					}
-	        					str +="<span class='subgroup1'>"+data
-
-[i].NAME+"("+data[i].FREQUENCY+")<input type='hidden' value='"+data[i].CHECKUPITEM_ID
-
-+"'></span>"+"/";
+	        					str +="<span class='subgroup1'>"+data[i].NAME+"("+data[i].FREQUENCY+")<input type='hidden' value='"+data[i].CHECKUPITEM_ID+"'></span>"+"/";
 	        				}
 	        			}
 	        			if(i==data.length-1){
@@ -133,36 +117,12 @@ md-12 col-sm-12'>";
 	    		        	var sub = MD5(sub1);
 	                    	//通过状态判断项目是否显示
 	    		        	 if(!(d.STATUS == "已删除")){
-	         		        	str+="<div onclick='onc(\"laiyuan"+sub+"\")' 
-
-id='laiyuan"+sub+"' class='zhiN source_adr col-lg-12 col-xs-12 col-md-12 col-sm-12' 
-
->"+d.DESCRIPTION+"</div>";
-	         		        	str += '<div class="deletli" id="'+sub+'"><a 
-
-href=\"javascript:del('+d.CHECKUPITEM_ID+',\''+d.STATUS+'\',\''+sub1+'\',\''+userId+'\')\" 
-
-id="'+d.CHECKUPITEM_ID+'" class="remove weui_btn weui_btn_mini weui_btn_primary rms" 
-
-style="float:right;"><img style="width: 1rem;height:1rem" src=\"../images/delete.png\" title=\"
-
-删除\" alt=\"删除\"/></a></div>';
+	         		        	str+="<div onclick='onc(\"laiyuan"+sub+"\")'id='laiyuan"+sub+"' class='zhiN source_adr col-lg-12 col-xs-12 col-md-12 col-sm-12'>"+d.DESCRIPTION+"</div>";
+	         		        	str += '<div class="deletli" id="'+sub+'"><ahref=\"javascript:del('+d.CHECKUPITEM_ID+',\''+d.STATUS+'\',\''+sub1+'\',\''+userId+'\')\"id="'+d.CHECKUPITEM_ID+'" class="remove weui_btn weui_btn_mini weui_btn_primary rms"style="float:right;"><img style="width: 1rem;height:1rem" src=\"../images/delete.png\" title=\"删除\" alt=\"删除\"/></a></div>';
 	         		        	str +='<div class="'+sub+'"></div>';
 	         		        }else {
-	         		        	str+="<div onclick='onc(\"laiyuan"+sub+"\")' 
-
-id='laiyuan"+sub+"' class='zhiN del source_adr col-lg-12 col-xs-12 col-md-12 col-sm-12' 
-
->"+d.DESCRIPTION+"</div>";
-	         		        	str += '<div class="deletli" id="'+sub+'"><a 
-
-href=\"javascript:del('+d.CHECKUPITEM_ID+',\''+d.STATUS+'\',\''+sub1+'\',\''+userId+'\')\" 
-
-id="'+d.CHECKUPITEM_ID+'" class="remove weui_btn weui_btn_mini weui_btn_primary hms" 
-
-style="float:right"><img style="width: 1rem;height:1rem" src=\"../images/huifu.png\" title=\"恢
-
-复\" alt=\"恢复\"/></a></div>';
+	         		        	str+="<div onclick='onc(\"laiyuan"+sub+"\")'id='laiyuan"+sub+"' class='zhiN del source_adr col-lg-12 col-xs-12 col-md-12 col-sm-12'>"+d.DESCRIPTION+"</div>";
+	         		        	str += '<div class="deletli" id="'+sub+'"><a href=\"javascript:del('+d.CHECKUPITEM_ID+',\''+d.STATUS+'\',\''+sub1+'\',\''+userId+'\')\" id="'+d.CHECKUPITEM_ID+'" class="remove weui_btn weui_btn_mini weui_btn_primary hms" style="float:right"><img style="width: 1rem;height:1rem" src=\"../images/huifu.png\" title=\"恢复\" alt=\"恢复\"/></a></div>';
 	         		        	str +='<div class="'+sub+' subgroup1"></div>';
 	         		        }
 	        		        str+="</div>";
@@ -173,17 +133,11 @@ style="float:right"><img style="width: 1rem;height:1rem" src=\"../images/huifu.p
 		       				 if (d.FEATURES != null && d.FEATURES !=''){
 		           					var birthArr= new Array();
 		           					//按逗号拆分
-		           					birthArr = d.FEATURES.split
-
-(",");
+		           					birthArr = d.FEATURES.split(",");
 		           					if(birthArr.length > 0){
 		           						var len = '';
-		           						for(var 
-
-p=0;p<birthArr.length;p++){
-		           							len += '<div 
-
-class="trait">'+birthArr[p]+'</div>';
+		           						for(var p=0;p<birthArr.length;p++){
+		           							len += '<div class="trait">'+birthArr[p]+'</div>';
 		           						}
 		           					}
 		           		    	}
@@ -198,9 +152,7 @@ class="trait">'+birthArr[p]+'</div>';
            					if(birthArr1.length > 0){
            					 var source = '';
            						for(var c=0 ; c<birthArr1.length;c++){
-           							source += '<div 
-
-class="zhi_source">'+birthArr1[c]+'</div>';
+           							source += '<div class="zhi_source">'+birthArr1[c]+'</div>';
            						}
            					}
 	        		    }
@@ -215,16 +167,10 @@ class="zhi_source">'+birthArr1[c]+'</div>';
 	
 }
 function onc(e){
-	
-	
 	if($("#"+e).css('maxHeight')=="40px"){
 		$("#"+e).css({maxHeight:"100%",overflow:"auto",display:"block"});
 	}else{
-		$("#"+e).css({maxHeight:"40px",overflow:"hidden",wordWrap:"break-
-
-word",textOverflow:"ellipsis",WebkitLineClamp:'2',textOverflow: "ellipsis",WebkitBoxOrient: 
-
-"vertical"});
+		$("#"+e).css({maxHeight:"40px",overflow:"hidden",wordWrap:"break-word",textOverflow:"ellipsis",WebkitLineClamp:'2',textOverflow: "ellipsis",WebkitBoxOrient: "vertical"});
 		$("#"+e).css({display:"-webkit-box"});
 	}
 }
@@ -282,20 +228,14 @@ $(document).delegate(".subgroup1",'click',function(){
 	$('.'+sub).append(len);
 	$('.'+sub).append(source);
 	$(this).parent().siblings("#laiyuan").html(DESCRIPTION);
-	$(this).parent().siblings(".deletli").find("a").attr("href","javascript:del('"+itemID
-
-+"','"+status+"','"+sub1+"','"+userId+"')");
+	$(this).parent().siblings(".deletli").find("a").attr("href","javascript:del('"+itemID+"','"+status+"','"+sub1+"','"+userId+"')");
 	$(this).parent().siblings(".deletli").find("a").attr("id",itemID);
 	if(status == '已选中'){
-		$(this).parent().siblings(".deletli").find("a").find("img").attr
-
-("src","../images/delete.png");
+		$(this).parent().siblings(".deletli").find("a").find("img").attr("src","../images/delete.png");
 		$(this).parent().siblings("#laiyuan").css('color','#000');
 		$(this).parent().siblings(".deletli").next("div").css('color','#000');
 	}else{
-		$(this).parent().siblings(".deletli").find("a").find("img").attr
-
-("src","../images/huifu.png");
+		$(this).parent().siblings(".deletli").find("a").find("img").attr("src","../images/huifu.png");
 		$(this).parent().siblings("#laiyuan").css('color','#C0BEBE');
 		$(this).parent().siblings(".deletli").next("div").css('color','#C0BEBE');
 	}
@@ -314,30 +254,20 @@ $(document).delegate(".subgroup1",'click',function(){
 			        contentType:"application/json;charset=utf8",
 			        data: JSON.stringify
 
-({"checkupItemId":ID,"stauts":status,"subGroup":group}),
+			        ({"checkupItemId":ID,"stauts":status,"subGroup":group}),
 			        dataType: "json",
 			        success: function (r) {
 			        	if (r.result == "success") {
 					        	if(status == '已删除'){
 					        		//$("#group_").find
 
-(".active").click(); 
-					        		$("#"+ID).attr
-
-("href","javascript:del('"+ID+"','已选中','"+userId+"')");
-					        		 $("#"+ID ).parent().find
-
-('#laiyuan').css('color','##8033C3');
+					        		(".active").click(); 
+					        		$("#"+ID).attr("href","javascript:del('"+ID+"','已选中','"+userId+"')");
+					        		 $("#"+ID ).parent().find('#laiyuan').css('color','##8033C3');
 					        	}else if(status == '已选中'){
-					        		 $('.weui_dialog_alert').css
-
-("display","none");
-					        		 $("#"+ID ).attr
-
-("href","javascript:del('"+ID+"','已删除','"+userId+"')");
-					        		 $("#"+ID ).parent().find
-
-('#laiyuan').css('color','red');
+					        		$('.weui_dialog_alert').css("display","none");
+					        		 $("#"+ID ).attr("href","javascript:del('"+ID+"','已删除','"+userId+"')");
+					        		 $("#"+ID ).parent().find('#laiyuan').css('color','red');
 					        	}
 			        		}
 			        	$('.xmtable').html('');
