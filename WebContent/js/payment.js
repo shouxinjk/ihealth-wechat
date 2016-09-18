f811bc7d6d452a5e32c0352a9eb28b512d0fb763
Yuan Jianglei <yuanjianglei@shouxinjk.com>
2016-09-18 21:25:56 +0800

    测试支付JS

--- WebContent/js/payment.js
+++ WebContent/js/payment.js
@@ -46,17 +46,17 @@ function onBridgeReady(data1,orderNo){
                 "signType":"MD5",
                 "paySign":data.paySign
             },
-            function(res){     
+          /*  function(res){     
                 if(res.err_msg =="get_brand_wcpay_request:ok") {
                 	window.location ="../subject/paymentresult.html?orderid="+orderNo;
                 	 alert("微信支付成功!");
                 }else if (res.err_msg == "get_brand_wcpay_request:cancel") {
                     alert("用户取消支付!"); 
                 }else{
-                    alert(res.err_msg);
-                    alert("支付失败!");
-                   /* alert(res.err_code);
-                    alert(res.err_desc);*/
+//                    alert(res.err_msg);
+//                    alert("支付失败!");
+                    alert(res.err_code);
+                    alert(res.err_desc);
                 }
             }
         );  
@@ -86,9 +86,24 @@ function WXPay(){
                          document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
                      }
                   }else{
-                     onBridgeReady(data.orderNo);
+                     onBridgeReady();
                   } 
 		}
 	});
-}
+}*/	
+            function(res){     
+                if(res.err_msg == "get_brand_wcpay_request：ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+            }
+        ); 
+     }
+     if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+        }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+        }
+     }else{
+        onBridgeReady();
+     }
             
