ee1a04e5f4ac3212e1437a7f50c702cc02d5f150
Yuan Jianglei <yuanjianglei@shouxinjk.com>
2016-09-22 01:21:00 +0800

    测试关心的人

--- WebContent/js/URL.js
+++ WebContent/js/URL.js
@@ -4,8 +4,9 @@ var wechatUrl="http://test.shouxinjk.net/ihealth-wechat";
 var addUrl = "http://test.shouxinjk.net";
 var urll = "http://test.shouxinjk.net/ihealth-wechat";
 
-//本地
-/*var url="http://localhost:8080/ihealth";
+/*//本地
+var url="http://localhost:8080/ihealth";
 var wechatUrl="http://test.shouxinjk.net/ihealth-wechat";
 var addUrl = "http://localhost:8080/";
-var urll = "http://localhost:8080/ihealth-wechat";*/
+var urll = "http://localhost:8080/ihealth-wechat";
+*/
