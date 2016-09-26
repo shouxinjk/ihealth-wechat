2d406e4acb8517c2130ab0db2637d8dec919df70
Yuan Jianglei <yuanjianglei@shouxinjk.com>
2016-09-26 14:42:16 +0800

    正式

--- WebContent/js/URL.js
+++ WebContent/js/URL.js
@@ -2,3 +2,4 @@ var url="http://www.shouxinjk.net/ihealth";
 var wechatUrl="http://www.shouxinjk.net/ihealth-wechat";
 var addUrl = "http://www.shouxinjk.net";
 var urll = "http://www.shouxinjk.net/ihealth-wechat";
+ 
